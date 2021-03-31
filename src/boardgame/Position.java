ac5c9be0a3a985a78486cd6d37b49a4f66f87db0
Alberto Schirmer <albertohenrique800@gmail.com>
2021-03-31 15:07:18 -0300

    First Class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,30 @@
+package boardgame;
+
+public class Position {
+
+	private int row, colum;
+
+	public Position(int row, int colum) {
+		this.row = row;
+		this.colum = colum;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColum() {
+		return colum;
+	}
+	public void setColum(int colum) {
+		this.colum = colum;
+	}
+
+	@Override
+	public String toString() {
+		return row +","+ colum;
+	}
+}
